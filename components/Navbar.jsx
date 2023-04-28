dbc44504b45f379974ce73e827b8401840745780
VishnuTamrakar <75554246+maheshtamarakar@users.noreply.github.com>
2023-04-28 09:31:51 +0530

    design almost completed

---
+++ components/Navbar.jsx
@@ -0,0 +1,90 @@
+import React, { useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  AiFillMinusCircle,
+  AiFillPlusCircle,
+  AiOutlineCloseCircle,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+import {
+  BsFillBagCheckFill
+} from "react-icons/bs";
+
+const Navbar = ({cart, addToCart, removeFromCart, clearCart, subTotal}) => {
+  const toggleCart = () => {
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else if (!ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
+    }
+  };
+  const ref = useRef();
+  return (
+    <>
+      <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md">
+        <div className="logo mx-5">
+          <Link href={"/"}>
+            <Image width={200} height={40} src="/myLogo.png" alt="" />
+          </Link>
+        </div>
+        <div className="nav">
+          <ul className="flex space-x-6 font-bold md:text-md">
+            <Link href={"/tshirts"}>
+              <li>Tshirts</li>
+            </Link>
+            <Link href={"/hoodies"}>
+              <li>Hoodies</li>
+            </Link>
+            <Link href={"/stickers"}>
+              <li>Stickers</li>
+            </Link>
+            <Link href={"/mugs"}>
+              <li>Mugs</li>
+            </Link>
+          </ul>
+        </div>
+        <div onClick={toggleCart} className="cart absolute right-0 mx-5 top-4">
+          <AiOutlineShoppingCart className="text-xl md:text-2xl cursor-pointer" />
+        </div>
+        <div
+          ref={ref}
+          className="w-72 h-full sideCart absolute top-0 right-0 bg-red-100 py-10 px-8 transform transition-transform translate-x-full"
+        >
+          <h2 className="font-bold text-xl text-center">Shopping Cart</h2>
+          <span
+            onClick={toggleCart}
+            className="absolute top-2 right-2 cursor-pointer text-2xl"
+          >
+            <AiOutlineCloseCircle />
+          </span>
+          <ol className="list-decimal font-semibold">
+            {Object.keys(cart).length === 0 &&
+            <div className="my-4 font-semibold">Your cart is Empty!</div>
+            }
+           {Object.keys(cart).map((k)=>{ return <li key={k}>
+              <div className="item flex my-5">
+                <div className="w-2/3 font-semibold">
+                  {cart[k].name}
+                </div>
+                <div className="flex font-semibold justify-center items-center w-1/3 text-sm">
+                  <AiFillMinusCircle onClick={()=>{removeFromCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant)}} className="cursor-pointer text-red-500" />
+                  <span className="mx-2">{cart[k].qty}</span>{" "}
+                  <AiFillPlusCircle onClick={()=>{addToCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant)}}  className="cursor-pointer text-red-500" />
+                </div>
+              </div>
+            </li>})}
+          </ol>
+          <div className="flex">
+          <button className="flex text-white bg-red-500 border-0 py-2 px-4 mr-2 focus:outline-none hover:bg-red-600 rounded text-sm"> <BsFillBagCheckFill className="m-1"/> Checkout</button>
+          <button onClick={clearCart} className="flex text-white bg-red-500 border-0 py-2 px-4 mr-2 focus:outline-none hover:bg-red-600 rounded text-sm"> Clear</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ pages/hoodies.js
@@ -0,0 +1,177 @@
+import React from "react";
+import Link from "next/link";
+
+const Hoodies = () => {
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+              <Link href={"/product/wear-the-anime"}>
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[30vh] md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61pCs6a0okL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </Link>
+
+              <div className="text-center md:text-left mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Anime
+                </h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+              <Link href={"/product/wear-the-anime"}>
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[30vh] md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61pCs6a0okL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </Link>
+
+              <div className="text-center md:text-left mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Anime
+                </h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+              <Link href={"/product/wear-the-anime"}>
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[30vh] md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61pCs6a0okL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </Link>
+
+              <div className="text-center md:text-left mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Anime
+                </h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+              <Link href={"/product/wear-the-anime"}>
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[30vh] md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61pCs6a0okL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </Link>
+
+              <div className="text-center md:text-left mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Wear The Anime
+                </h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+              <Link href={"/product/wear-the-anime"}>
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[30vh] md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61pCs6a0okL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </Link>
+
+              <div className="text-center md:text-left mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Anime
+                </h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+              <Link href={"/product/wear-the-anime"}>
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[30vh] md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61pCs6a0okL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </Link>
+
+              <div className="text-center md:text-left mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Anime
+                </h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+              <Link href={"/product/wear-the-anime"}>
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[30vh] md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61pCs6a0okL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </Link>
+
+              <div className="text-center md:text-left mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Anime
+                </h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+              <Link href={"/product/wear-the-anime"}>
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[30vh] md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61pCs6a0okL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </Link>
+
+              <div className="text-center md:text-left mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Wear The Anime
+                </h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Hoodies;
